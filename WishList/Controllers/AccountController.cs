c78706bb05725ce1f8ae9c34f15b1a554cbb1347
Nyalls Hemingway <nyalls.hemingway@scot.gov>
2020-05-21 14:42:24 +0000

    added functionality for registering

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault.Models;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
@@ -24,6 +26,45 @@ namespace WishList.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(RegisterViewModel registerViewModel) 
+        {
+            if (ModelState.IsValid)
+            {
+                var newUser = new ApplicationUser()
+                {
+                    UserName = registerViewModel.Email,
+                    Email = registerViewModel.Email,
+                };
+                var password = registerViewModel.Password;
+
+                var identityResult = _userManager.CreateAsync(newUser, password);
+                if (!identityResult.Result.Succeeded)
+                {
+                    foreach(var error in identityResult.Result.Errors)
+                    {
+                        ModelState.AddModelError("Password", error.Description);
+                    }
+                }
+
+                return View("Register", registerViewModel);
+            }
+            else
+            {
+                return View("Register", registerViewModel);
+            }
+
+            return RedirectToAction("HomeController.Index");
         }
     }
 }
